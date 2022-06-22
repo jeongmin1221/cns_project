55e001a13977142de397a8bbf16781b652cf1c88
Nayeon Kim <kny010901@naver.com>
2022-06-22 21:04:59 +0900

    first commit

---
+++ app/src/main/java/com/example/cns_456/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cns_456;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cns_4p_01);
+    }
+}
